d77f452e29f5bae325ec7c59b0461c36627c87b5
Vaibhav Mali <vaibhavmali1712@gmail.com>
2026-09-03 09:15:56 +0530

    Fix argument in ResourceNotFoundException

--- empManSystem/src/main/java/com/practiceEmployee/empManSystem/exception/ResourceNotFoundException.java
+++ empManSystem/src/main/java/com/practiceEmployee/empManSystem/exception/ResourceNotFoundException.java
@@ -5,13 +5,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-		
 	private String resourceName;
 	private String fieldName;
 	private Object fieldValue;
 	
 	public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue)
-	 {  super(String.format("%s not found with %s : %s ", resourceName, fieldName,fieldValue));
+	 {  super(String.format("%s not found with %s : %s ",fieldValue,fieldName,resourceName));
 		this.resourceName = resourceName;
 		this.fieldName = fieldName;
 		this.fieldValue = fieldValue;
